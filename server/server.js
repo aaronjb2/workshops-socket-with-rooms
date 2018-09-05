ab0c2fa939913f7e8a88b26e7d844de68af490f9
tmat06 <timmy.a.matthews@gmail.com>
2018-09-05 10:43:07 -0600

    completed socket with rooms

--- server/server.js
+++ server/server.js
@@ -9,7 +9,34 @@ const io = socket(
 
 io.on("connection", socket => {
   // io.engine.clientsCount Will give the number of connected users to the socket
-  socket.on("workshop-socket-listener", data => {
-    socket.emit("client-listener", { name: data.name });
+
+  socket.on("join-chat", data => {
+    // The Broadcast flag will send to all sockets except the one that sent the message
+    socket.broadcast.emit("all-users", data.name);
+  });
+
+  socket.on("send-message", data => {
+    //io.emit will broadcast the message to all users
+    io.emit("message-received", data);
+  });
+
+  socket.on("join-room", data => {
+    // this will join the socket id to whatever room was passed to it, therefore will have access to all future messages emitted to this room until leave()
+    socket.join(data.room);
+    // Need to specify which room this message will be emitted to
+    io.to(data.room).emit("room-message-received", {
+      message: `new user to room ${data.room}`
+    });
+  });
+
+  socket.on("send-room-message", data => {
+    // to and in are interchangeable
+    io.in(data.room).emit("send-room-message-received", {
+      message: `${data.name} says: ${data.message}`
+    });
+  });
+
+  io.on("disconnect", () => {
+    console.log("he out");
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,33 +1,161 @@
-import React from "react";
+import React, { Component } from "react";
+import "./App.css";
 import io from "socket.io-client";
 
+// Sockets have traditionally been the solution around which most realtime chat systems are architected,
+// providing a bi-directional communication channel between a client and a server.
+// This means that the server can push messages to clients. Whenever you write a chat message,
+// the idea is that the server will get it and push it to all other connected clients.
+
+//Socket.IO is composed of two parts:
+
+//A server that integrates with (or mounts on) the Node.JS HTTP Server: socket.io
+//A client library that loads on the browser side: socket.io-client
+
+// Need to initially tell the socket what server it will be listening to, when hosted, you'll erase the http://localhost:3007 because the server will be serving static files
 const socket = io.connect("http://localhost:3007");
 
-export default class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: ""
+      joined: false,
+      roomJoined: false,
+      name: "",
+      message: "",
+      roomMessage: "",
+      messages: [],
+      roomMessages: [],
+      room: ""
     };
 
-    socket.on("client-listener", data => {
-      console.log(data);
+    ///////////////////global chat/////////////////////////////////////
+    socket.on("all-users", data => {
+      this.setState(() => {
+        let tempMessages = [...this.state.messages];
+        tempMessages.push(`${data} has joined the chat`);
+        return { messages: tempMessages };
+      });
+    });
+
+    socket.on("message-received", data => {
+      this.setState(() => {
+        let tempMessages = [...this.state.messages];
+        tempMessages.push(`${data.name} says: ${data.message}`);
+        return { messages: tempMessages };
+      });
+    });
+    //////////////////////////////////////////////////////////////////
+
+    /////////////////////// room chat /////////////////////////////
+
+    socket.on("room-message-received", data => {
+      let tempMessages = [...this.state.roomMessages];
+      tempMessages.push(data.message);
+      this.setState({ roomMessages: tempMessages });
+    });
+
+    socket.on("send-room-message-received", data => {
+      this.setState(() => {
+        let tempMessages = [...this.state.roomMessages];
+        tempMessages.push(data.message);
+        return { roomMessages: tempMessages };
+      });
+    });
+    ////////////////////////////////////////////////////////////
+  }
+
+  joinChat() {
+    this.setState({ joined: true });
+
+    socket.emit("join-chat", { name: this.state.name });
+  }
+
+  sendMessage() {
+    socket.emit("send-message", {
+      name: this.state.name,
+      message: this.state.message
     });
   }
 
-  sendSocketMessage() {
-    socket.emit("workshop-socket-listener", { name: this.state.name });
+  joinRoom() {
+    this.setState({ roomJoined: true });
+
+    socket.emit("join-room", { room: this.state.room });
+  }
+
+  sendRoomMessage() {
+    // because we'll be sending it to a specific room, we need to pass what room it will be going to
+    socket.emit("send-room-message", {
+      name: this.state.name,
+      room: this.state.room,
+      message: this.state.roomMessage
+    });
   }
 
   render() {
     return (
-      <div>
-        <input
-          onChange={e => this.setState({ name: e.target.value })}
-          placeholder="name"
-        />
-        <button onClick={() => this.sendSocketMessage()}>Submit</button>
+      <div className="App">
+        {/* Global Chat */}
+        <div className="chat-box">
+          {this.state.messages.map((val, i) => {
+            return (
+              <div key={i}>
+                <h3>{val}</h3>
+              </div>
+            );
+          })}
+          {/* Changes input and buttons based on whether they've initially joined */}
+          {!this.state.joined ? (
+            <div>
+              <input
+                placeholder="username"
+                onChange={e => this.setState({ name: e.target.value })}
+              />
+              <button onClick={() => this.joinChat()}>Submit</button>
+            </div>
+          ) : (
+            <div>
+              <input
+                placeholder="send message"
+                onChange={e => this.setState({ message: e.target.value })}
+              />
+              <button onClick={() => this.sendMessage()}>Send</button>
+            </div>
+          )}
+        </div>
+
+        {/* Room Chat */}
+        <div className="chat-box">
+          {this.state.roomMessages.map((val, i) => {
+            return (
+              <div key={i}>
+                <h3>{val}</h3>
+              </div>
+            );
+          })}
+          {/* Changes based on whether they've joined a room or not */}
+          {!this.state.roomJoined ? (
+            <div>
+              <input
+                placeholder="join a room"
+                onChange={e => this.setState({ room: e.target.value })}
+              />
+              <button onClick={() => this.joinRoom()}>Join Room</button>
+            </div>
+          ) : (
+            <div>
+              <input
+                placeholder="send message"
+                onChange={e => this.setState({ roomMessage: e.target.value })}
+              />
+              <button onClick={() => this.sendRoomMessage()}>Send</button>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
 }
+
+export default App;
